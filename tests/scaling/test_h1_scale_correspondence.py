2fe2837c25acc5bfef5dfdcb68aefd079102efc9
robinbishtt <robinbisht49@gmail.com>
2026-03-02 07:28:33 +0000

    Updated scaling law parameters.

--- tests/scaling/test_h1_scale_correspondence.py
+++ tests/scaling/test_h1_scale_correspondence.py
@@ -15,3 +15,4 @@ def test_chi1_below_one_implies_decay():
     from src.core.correlation.two_point import chi1_gauss_hermite
     chi1 = chi1_gauss_hermite(sigma_w2=1.0, nonlinearity="tanh")
     assert chi1 < 1.0 or chi1 > 0.0  # exists and is finite
+ 
